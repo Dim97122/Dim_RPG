c294a7329791774ed707917a2168e3f48ac83a9d
Dim <dimitri.kiavue@gmail.com>
2020-12-06 15:07:05 +0100

    [ADD] StaticPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :characters
   resources :turns
+  resources :games
+
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
