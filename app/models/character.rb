6e6ef19edf1f78e34a15bcd60b1664cde4931b54
Dim <dimitri.kiavue@gmail.com>
2020-12-06 15:01:49 +0100

    [UPDATE] Validations for Character & Turn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,15 @@
 class Character < ApplicationRecord
+  validates :name, presence: true
+  validates :life_points, presence: true, numericality: {
+     only_integer: true,
+     greater_than_or_equal_to: 75,
+     less_than_or_equal_to: 125
+   }
+   validates :attack_points, presence: true, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 25,
+      less_than_or_equal_to: 50
+    }
   has_many :turns
   has_many :games, through: :turns
 end
--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,4 +1,7 @@
 class Turn < ApplicationRecord
+  validates :game_id, presence: true, numericality: { only_integer: true }
+  validates :home_character_life_points, presence: true, numericality: { only_integer: true }
+  validates :away_character_life_points, presence: true, numericality: { only_integer: true }
   belongs_to  :game
   belongs_to  :home_character, class_name: "Character"
   belongs_to  :away_character, class_name: "Character"
