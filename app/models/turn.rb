13a18ff32ea97f3a7d2cceafdf771f65d1e133e3
Dim <dimitri.kiavue@gmail.com>
2020-12-06 21:02:39 +0100

    [ADD] New Play method

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -5,4 +5,19 @@ class Turn < ApplicationRecord
   belongs_to  :game
   belongs_to  :home_character, class_name: "Character"
   belongs_to  :away_character, class_name: "Character"
+
+  def new_play
+    current_game = game
+    if plays == 2
+      current_game.turns.create!(
+        home_character: home_character,
+        home_character_life_points: home_character.life_points,
+        away_character: away_character,
+        away_character_life_points: away_character.life_points,
+        plays: 0,
+      )
+    else
+      update(plays: plays + 1)
+    end
+  end
 end
