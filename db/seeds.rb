76373152cd8d29d8599a1bd1ca04f446154f8fd4
Dim <dimitri.kiavue@gmail.com>
2020-12-06 17:47:08 +0100

    [ADD] Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Character.destroy_all
+Game.destroy_all
+Turn.destroy_all
+
+5.times do
+  Character.create!(
+    name: Faker::Name.first_name,
+    life_points: Faker::Number.between(from: 75, to: 125),
+    attack_points: Faker::Number.between(from: 25, to: 50)
+  )
+end
