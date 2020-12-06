5454a4934784cde802304fda71574bb0ea9968ae
Dim <dimitri.kiavue@gmail.com>
2020-12-06 15:29:25 +0100

    [UPDATE] module import and export

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,12 @@
-import { environment } from '@rails/webpacker';
-import { ProvidePlugin } from 'webpack';
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 
 environment.plugins.append('Provide',
-  new ProvidePlugin({
+  new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js, default'],
   })
 )
 
-export default environment
+module.exports = environment
